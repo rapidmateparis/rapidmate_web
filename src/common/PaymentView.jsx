c6df4e8288a4cb9569dcdbd17debcfdcae085ec6
avaleenbrush <evaleenblush@awgarstone.com>
2025-01-23 20:12:48 +0530

    update code 23-01-2025

--- src/common/PaymentView.jsx
+++ src/common/PaymentView.jsx
@@ -34,6 +34,7 @@ import { ToastContainer } from "react-toastify";
 import { showErrorToast, showSuccessToast } from "../utils/Toastify";
 import { addLocation, BASE_URL, localToUTC, uploadImage } from "../utils/Constants";
 import PickupAddPaymentMethodsModal from "../components/consumer/account/PickupAddPaymentMethodsModal";
+import localforage from "localforage";
 
 const stripePromise = loadStripe(
   "pk_test_51PgiLhLF5J4TIxENPZOMh8xWRpEsBxheEx01qB576p0vUZ9R0iTbzBFz0QvnVaoCZUwJu39xkym38z6nfNmEgUMX00SSmS6l7e"
@@ -577,12 +578,13 @@ function PaymentView() {
   useEffect(() => {
     if (paymentAmount > 0) {
       const createPaymentIntent = async () => {
+        const token = await localforage.getItem('1');
         try {
           const response = await fetch(
             `${BASE_URL}payment/create-payment-intent`,
             {
               method: "POST",
-              headers: { "Content-Type": "application/json" },
+              headers: { "Content-Type": "application/json",'Authorization': token },
               body: JSON.stringify({
                 amount: paymentAmount, // Convert to cents for Stripe
                 currency: "eur",
